ba038508239155f59bfbe916bfb07d9722108387
p0wered <powered.ui@gmail.com>
2023-11-06 22:50:58 +0300

    вывод карточек вынесен в класс GameBoard

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 import random
-from typing import Optional
+import pygame as pg
 from card import GameCard
 
 
@@ -13,6 +13,23 @@ class GameBoard:
     def __repr__(self):
         return str(self.cards)
 
+    @staticmethod
+    def render(display, custom_list):
+        """Выводит на экран сетку карточек 2х4 в случайном порядке"""
+        card_cloud, card_tree, card_blob, card_flower = pg.image.load('images/blue.png'), pg.image.load('images/red.png'), pg.image.load('images/yellow.png'), pg.image.load('images/purple.png')
+        cycle, shift = 0, 0
+        for card in custom_list:
+            if cycle % 4 == 0:
+                shift += 142
+                cycle = 0
+            cycle += 1
+            card = str(card)
+            if card == 'card_cloud':
+                display.blit(card_cloud, (cycle * 140, shift))
+            elif card == 'card_tree':
+                display.blit(card_tree, (cycle * 140, shift))
+            elif card == 'card_blob':
+                display.blit(card_blob, (cycle * 140, shift))
+            else:
+                display.blit(card_flower, (cycle * 140, shift))
 
-board = GameBoard
-print(board.cards)
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import random
 import pygame as pg
 from card import GameCard
+from board import GameBoard
 
 pg.init()
 
@@ -14,10 +15,6 @@ pg.display.set_caption('Purble Pairs')
 
 # картинки
 bg_img = pg.image.load('src/bg.jpg')
-card_cloud = pg.image.load('images/blue.png')
-card_tree = pg.image.load('images/red.png')
-card_blob = pg.image.load('images/yellow.png')
-card_flower = pg.image.load('images/purple.png')
 
 # переменные
 display = pg.display.set_mode((screen_width, screen_height))
@@ -25,27 +22,14 @@ running = True
 clock = pg.time.Clock()
 cards = GameCard.all_cards()
 random.shuffle(cards)
+col_cloud_1 = pg.Rect(0, 0, 0, 0)
 
 
 # экран
 def display_redraw():
-    cycle = 0
-    shift = 0
     display.blit(bg_img, (0, 0))
-    for card in cards:
-        if cycle % 4 == 0:
-            shift += 142
-            cycle = 0
-        cycle += 1
-        card = str(card)
-        if card == 'card_cloud':
-            display.blit(card_cloud, (cycle * 140, shift))
-        elif card == 'card_tree':
-            display.blit(card_tree, (cycle * 140, shift))
-        elif card == 'card_blob':
-            display.blit(card_blob, (cycle * 140, shift))
-        else:
-            display.blit(card_flower, (cycle * 140, shift))
+    main_board = GameBoard
+    main_board.render(display, cards)
     pg.display.update()
 
 
@@ -59,7 +43,7 @@ def event_processing():
             if event.key == pg.K_q:
                 running = False
         if event.type == pg.MOUSEBUTTONUP:
-            print(pg.mouse.get_pos())
+            print('mouse clicked')
     clock.tick(FPS)
     return running
 
