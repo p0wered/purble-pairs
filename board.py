d7afcc5ea3bdb95d4cc7ed9a45b90965ac52d618
p0wered <powered.ui@gmail.com>
2023-11-07 21:18:31 +0300

    - заменён фон - карточки выравнены - добавлен список для хранения коллизий

--- board.py
+++ board.py
@@ -14,22 +14,26 @@ class GameBoard:
         return str(self.cards)
 
     @staticmethod
-    def render(display, custom_list):
+    def render(display, sprites, positions):
         """Выводит на экран сетку карточек 2х4 в случайном порядке"""
         card_cloud, card_tree, card_blob, card_flower = pg.image.load('images/blue.png'), pg.image.load('images/red.png'), pg.image.load('images/yellow.png'), pg.image.load('images/purple.png')
-        cycle, shift = 0, 0
-        for card in custom_list:
-            if cycle % 4 == 0:
-                shift += 142
+        card_width, card_height = 128, 128
+        st_x, st_y, cycle = 261, 244, 0
+        for item in sprites:
+            if cycle // 4 in [1, 2, 3]:
+                st_x = 261
+                st_y += card_height + 10
                 cycle = 0
             cycle += 1
-            card = str(card)
-            if card == 'card_cloud':
-                display.blit(card_cloud, (cycle * 140, shift))
-            elif card == 'card_tree':
-                display.blit(card_tree, (cycle * 140, shift))
-            elif card == 'card_blob':
-                display.blit(card_blob, (cycle * 140, shift))
+            item = str(item)
+            if len(positions) < 8:
+                positions.append((st_x, st_y))
+            st_x += card_width + 10
+            if item == 'card_cloud':
+                display.blit(card_cloud, (st_x, st_y))
+            elif item == 'card_tree':
+                display.blit(card_tree, (st_x, st_y))
+            elif item == 'card_blob':
+                display.blit(card_blob, (st_x, st_y))
             else:
-                display.blit(card_flower, (cycle * 140, shift))
-
+                display.blit(card_flower, (st_x, st_y))
--- game.py
+++ game.py
@@ -8,13 +8,13 @@ pg.init()
 # параметры
 FPS = 60
 screen_width, screen_height = 1366, 768
-pg.mixer.music.load('src/ambient.mp3')
+pg.mixer.music.load('src/disco.mp3')
 pg.mixer.music.play()
 pg.mixer.music.set_volume(0.3)
 pg.display.set_caption('Purble Pairs')
 
 # картинки
-bg_img = pg.image.load('src/bg.jpg')
+bg_img = pg.image.load('src/bg.png')
 
 # переменные
 display = pg.display.set_mode((screen_width, screen_height))
@@ -22,14 +22,14 @@ running = True
 clock = pg.time.Clock()
 cards = GameCard.all_cards()
 random.shuffle(cards)
-col_cloud_1 = pg.Rect(0, 0, 0, 0)
+positions = []
 
 
 # экран
 def display_redraw():
     display.blit(bg_img, (0, 0))
     main_board = GameBoard
-    main_board.render(display, cards)
+    main_board.render(display, cards, positions)
     pg.display.update()
 
 
